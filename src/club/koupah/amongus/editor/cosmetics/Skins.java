bf331a2f054fc5544bcf67d9ba2b488609304270
TheOnlyNooc <coopern@students.igssyd.nsw.edu.au>
2020-09-19 21:14:09 +1000

    Update Skins.java

--- src/club/koupah/amongus/editor/cosmetics/Skins.java
+++ src/club/koupah/amongus/editor/cosmetics/Skins.java
@@ -5,7 +5,15 @@ import club.koupah.amongus.editor.cosmetics.Cosmetic.CosmeticType;
 public enum Skins {
 
 	None(0),
-	
+	Astronaut(1),
+	Captain(2),
+	Mechanic(3),
+	Military(4),
+	Police(5),
+	Scientist(6),
+	Black_Suit(7),
+	White_Suit(8),
+	Wall_Guard(9),
 	Orange_Karate(10),
 	Weird_Blue_Suit(11),
 	Construction_Worker(12),
