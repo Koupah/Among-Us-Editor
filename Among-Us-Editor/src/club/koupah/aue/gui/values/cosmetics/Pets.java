272bdd5d726f7ec02ffd382d51e1d19ea3dc16f6
Cory Sanin <corysanin@outlook.com>
2020-12-04 19:27:08 -0600

    Address PR feedback
    Add Promotional category, bump version number, update the string for Seasonal + Paid

--- Among-Us-Editor/src/club/koupah/aue/gui/values/cosmetics/Pets.java
+++ Among-Us-Editor/src/club/koupah/aue/gui/values/cosmetics/Pets.java
@@ -25,7 +25,7 @@ public enum Pets {
 	Ellie(8, Paid),
 	Squiq(9, Paid),
 	Bedcrab(10, Paid),
-	Twitch(11, Paid);
+	Twitch(11, Promotional);
 	
 	Pets(int ID, CosmeticCategory category) {
 		new Cosmetic(this,ID,CosmeticType.Pet, category);
