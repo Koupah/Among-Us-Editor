80ab2089eb97c75cd353238421236a9c1aeaa351
Koupah <ktayl21@yahoo.com.au>
2021-03-10 14:05:19 +1000

    Removed Old Skeld.net Servers

--- Among-Us-Editor/src/club/koupah/aue/gui/types/impl/custom/servers/DefaultServers.java
+++ Among-Us-Editor/src/club/koupah/aue/gui/types/impl/custom/servers/DefaultServers.java
@@ -2,8 +2,7 @@ package club.koupah.aue.gui.types.impl.custom.servers;
 
 public enum DefaultServers {
 	
-	SkeldDotNetUS("Skeld.net US Server", "44.238.242.123"),
-	SkeldDotNetEU("Skeld.net EU Server", "92.237.178.55");
+	SkeldDotNetUS("Skeld.net US Server", "192.241.154.115");
 	
 	DefaultServers(String name, String ip) {
 		this(name, ip, (short)22023);
